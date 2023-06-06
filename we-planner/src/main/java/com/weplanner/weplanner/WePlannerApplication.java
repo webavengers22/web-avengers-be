69442b6909469ca0b3894af37037f9d007dc29bc
Haerim <71517226+kimhaerim@users.noreply.github.com>
2023-06-06 22:25:07 +0900

    기초 세팅

---
+++ we-planner/src/main/java/com/weplanner/weplanner/WePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.weplanner.weplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WePlannerApplication.class, args);
+	}
+
+}
---
+++ we-planner/src/test/java/com/weplanner/weplanner/WePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weplanner.weplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
