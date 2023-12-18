755c6b952e9d337172d0399780b592ea2ba5aa3f
Haerim <71517226+kimhaerim@users.noreply.github.com>
2023-12-18 22:32:57 +0900

    공통 사용 개발

---
+++ we-planner/src/main/java/com/weplanner/weplanner/api/common/CommonResponse.java
@@ -0,0 +1,7 @@
+package com.weplanner.weplanner.api.common;
+
+public class CommonResponse {
+    boolean success;
+    int code;
+    String message;
+}
---
+++ we-planner/src/main/java/com/weplanner/weplanner/api/common/ListResponse.java
@@ -0,0 +1,7 @@
+package com.weplanner.weplanner.api.common;
+
+import java.util.List;
+
+public class ListResponse <T> extends CommonResponse{
+    List<T> dataList;
+}
